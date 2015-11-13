242afcda53602f6bdc9baabd842deeb01641f376
nikitatihonov <email.address@example.net>
2015-11-13 10:02:11 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -1,12 +1,13 @@
 <?php
-
 // включим отображение всех ошибок
-error_reporting(E_ALL);
+error_reporting (E_ALL); 
+
 // подключаем конфиг
-include ('/config.php');
+include ('../config.php'); 
 
 // Соединяемся с БД
-$dbObject = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+$dbObject = new PDO('pgsql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+$dbObject->exec('SET CHARACTER SET utf8');
 
 // подключаем ядро сайта
 include (SITE_PATH . DS . 'core' . DS . 'core.php');
